8a83864b23f8396716d9e7ade8710df21f3e4bd0
Nathaniel Wu <55820966+nwu124@users.noreply.github.com>
2023-08-06 14:31:16 -0700

    Update noise.py

--- code/noise.py
+++ code/noise.py
@@ -1,34 +1,20 @@
-# From https://github.com/talonvoice/examples/blob/master/noise.py
-import time
+"""
+Map noises (like pop) to actions so they can have contextually differing behavior
+"""
 
-from talon import ctrl
-from talon import tap
-from talon import noise
-from talon.track.geom import Point2d
+from talon import Module, actions, noise, ctrl
 
-class NoiseModel:
-    def __init__(self):
-        self.button = 0
-        self.mouse_origin = Point2d(0, 0)
-        self.mouse_last = Point2d(0, 0)
-        self.dragging = False
+mod = Module()
 
-        tap.register(tap.MMOVE, self.on_move)
-        noise.register('pop', self.on_pop)
 
-    def on_move(self, typ, e):
-        if typ != tap.MMOVE: return
-        self.mouse_last = pos = Point2d(e.x, e.y)
-        if not self.dragging:
-            if (pos - self.mouse_origin).len() > 10:
-                self.dragging = True
-                self.button = 0
-                x, y = self.mouse_origin.x, self.mouse_origin.y
-                ctrl.mouse(x, y)
-                ctrl.mouse_click(pos = (x, y), button=0, down=True)
-
-    def on_pop(self, noise):
-        now = time.time()
+@mod.action_class
+class Actions:
+    def noise_trigger_pop():
+        """
+        Called when the user makes a 'pop' noise. Listen to
+        https://noise.talonvoice.com/static/previews/pop.mp3 for an
+        example.
+        """
         ctrl.mouse_click(button=0, hold=16000)
 
-model = NoiseModel()
+noise.register("pop", lambda _: actions.user.noise_trigger_pop())
